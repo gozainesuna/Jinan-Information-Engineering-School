1ad0fe7e4f19a74dbb888d213b036516c73e3963
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-09-12 21:00:05 +0800

    创建 3-3.c

---
+++ 3-3.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+void main()
+{
+	int a,b,c,min;
+	
+	printf("输入a,b,c三个整数：");
+	scanf("%d %d %d",&a,&b,&c);
+	if(a<b)
+		min = a ; 
+	else
+		min = b ;
+	if(c<min)
+		min = c ;
+	
+	printf("最小者 = %d \n",min);
+}
