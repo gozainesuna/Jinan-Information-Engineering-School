4bc8c65657fb347267242e8ee5764b001719e60e
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-09-12 22:22:16 +0800

    例题3-4

---
+++ 3-4.c
@@ -0,0 +1,31 @@
+﻿#include<stdio.h>
+void main()
+{
+	int a,b,c,tmp;
+	
+	printf("输入a,b,c三个整数：");
+	scanf("%d %d %d",&a,&b,&c);
+	
+	if(a>b)
+	{
+		tmp = a;
+		a = b;
+		b = tmp;
+	}
+	
+	if(a>c)
+	{
+		tmp = a;
+		a = c;
+		c = tmp;
+	}
+	
+	if(b>c)
+	{
+		tmp = b;
+		b = c;
+		c = tmp;
+	}
+	
+	printf("%d %d %d\n",a,b,c);
+}
