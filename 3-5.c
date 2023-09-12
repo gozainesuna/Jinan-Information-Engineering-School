aa417043ce61645d8539f81cbd6a374bc426f1c7
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-09-12 22:34:07 +0800

    例题3-5 利用变长求得三角形周长

---
+++ 3-5.c
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+void main()
+{
+	float a,b,c;
+	
+	scanf("%f %f %f",&a,&b,&c);	
+	if(a+b>c && a+c>b && b+c>a)
+		printf("三角形的周长为：%7.2f\n",a+b+c);
+	else
+		printf("error!\n");
+}
