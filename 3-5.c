96aacce9bcc297d7ae56de703568e4255c47d022
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-09-13 18:40:17 +0800

    补充注释

--- 3-5.c
+++ 3-5.c
@@ -1,9 +1,11 @@
-﻿#include<stdio.h>
+﻿//本程序用于，三角形中，输入三边长度，输出三角形周长
+#include<stdio.h>
 void main()
 {
 	float a,b,c;
 	
 	scanf("%f %f %f",&a,&b,&c);	
+	
 	if(a+b>c && a+c>b && b+c>a)
 		printf("三角形的周长为：%7.2f\n",a+b+c);
 	else
