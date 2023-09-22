3f35e77c9d13ea4b1f027a4fa902c60d6da18983
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-09-22 22:08:03 +0800

    例题4-5.在键盘上输入若干个证书，直到输入0为止，求它们的乘积

---
+++ 例题 4-5.c
@@ -0,0 +1,27 @@
+﻿//在键盘上输入若干个证书，直到输入0为止，求它们的乘积
+#include<stdio.h>
+void main()
+{
+	int n ;
+	long t = 1 ;
+	
+	printf("输入若干个整数，指代输入0为止：\n");
+	scanf("%d",&n);
+	
+	if( n==0 )
+	{
+		t = 0;
+		printf("%ld",t);
+	}
+	
+	else
+	{
+		while( n!=0 ) 
+		 {
+		 	t = t*n;
+		 	scanf("%d",&n);
+		 }
+		 
+		 printf("%ld\n",t);
+	}
+}
