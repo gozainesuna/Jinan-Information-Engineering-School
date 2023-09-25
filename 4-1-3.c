d5b85f8f21842c921e9d5c22b066ac1e823e2e8f
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-09-25 17:59:46 +0800

    创建

---
+++ 4-1-3.c
@@ -0,0 +1,18 @@
+﻿//求1+3+5+7+9......+99（100以内奇数）之和
+#include<stdio.h>
+void main()
+{
+	int sum;//记录累加数值
+	int i;//
+	
+	sum = 0;
+	i = 1;
+	
+	while( i<99 );
+	{
+		sum = sum+i;
+		i = i+2;
+	}
+	
+	printf("1+3+5+7+9......+99 = %d\n",sum);
+}
---
+++ 4-1-4.c
@@ -0,0 +1,18 @@
+﻿//100以内所有偶数之和
+#include<stdio.h>
+void main()
+{
+	int sum;
+	int i;
+	
+	sum = 0;
+	i = 0;
+	
+	while( i<=100)
+	{
+		sum = sum+i;
+		i = i+2;
+	}
+	
+	printf("2+4+6+8+10......+100 = %d\n",sum);
+}
