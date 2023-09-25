be468d96df555b1d07096d3bd9684722512221ee
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-09-25 19:43:59 +0800

    1

---
+++ 4-2-2.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+void main()
+{
+	int sum;
+	int num;
+	int i;
+	
+	printf("从键盘上输入10个整数：\n")
+	
+	sum = 0;
+	
+	for( i=1; i<=10; i++;)
+	{
+		scanf("%d",&num);
+		sum = sum+num;
+	}
+	
+	printf("这十个整数的和为：%d\n",sum);
+}
---
+++ 4-4-3.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+void main()
+{
+	int sum;
+	int num;
+	int i;
+	
+	printf("从键盘上输入10个整数：\n")；
+	sum = 0;
+	i = 1;
+	
+	while( i<=10 );
+	{
+		scanf("%d",&num);
+		sum = sum+sum;
+		i++;
+	}
+	
+	printf("这10个整数的和为：%d\n",sum);
+}
