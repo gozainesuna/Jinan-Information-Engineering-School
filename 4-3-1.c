7a568745c4504b75db0197a281ea793562f26193
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-09-25 19:25:59 +0800

    创建

---
+++ 4-3-1.c
@@ -0,0 +1,20 @@
+﻿//输入一个整数，并按照相反顺序输出。例如输入12345，输出54321
+#include<stdio.h>
+void main()
+{
+	int num;//存储中间值
+	int b;
+	
+	printf("输入一个整数：");
+	scanf("%d",&num);
+	
+	do
+	{
+		b = num%10;
+		printf("%d",b);
+		num = num/10;
+	}
+	while(num);
+	
+	printf("\n");
+}
---
+++ 4-3-2.c
@@ -0,0 +1,19 @@
+﻿//输入一个整数，并按照相反顺序输出。例如输入12345，输出54321
+#include<stdio.h>
+void main()
+{
+	int num;
+	int b;
+	
+	ptinrf("输入一个整数：");
+	scanf("%d",&num);
+	
+	while(num);
+	{
+		b = num%10;
+		printf("%d",b);
+		num = num/10;	
+	}
+	
+	printf("\n");
+}
