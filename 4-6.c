0be978c5be39224b69cd225acc3858cfa5995058
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-09-25 20:00:40 +0800

    创建 4-6.c

---
+++ 4-6.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+void main()
+{
+	int i;
+	int j;
+	
+	for( i=1;	i<=9;	i++; )
+	{
+		printf("%5d",i);
+	}
+	
+	printf("\n------------------------------------\n");
+	
+	for( i=1;	i<=9;	i++; )
+	{
+		for( j=1;	j<=1;	j++ )
+		{
+			printf("%2d*%d = %-3d",i,j,i*j);
+		}
+		
+		printf("\n");
+	}
+}
