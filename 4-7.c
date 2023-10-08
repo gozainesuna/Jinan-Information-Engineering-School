2291d4ddb25599ee5c1b717c9121b223d9ad48ea
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-10-08 21:17:12 +0800

    添加

---
+++ 4-7.c
@@ -0,0 +1,27 @@
+﻿//例题4-7
+//求不同半径的圆柱体体积，只计算体积在100以下的圆柱体
+#include<stdio.h>
+#define PI 3.14
+void main()
+{
+	int r;
+	float h;
+	double v;
+	
+	printf("请输入圆柱体的高：");
+	scanf("%f",&h);
+	
+	for ( r=1; r<=10; r++ )
+	{
+		v = PI*r*r*h;
+		
+		if( v>=100 )
+		{
+			break;
+		}
+		
+		printf("半径等于%d,体积等于%.2f\n",r,v);
+	}
+	
+	printf("此时r=%d\n",r);
+}
---
+++ 4-8-1.c
@@ -0,0 +1,22 @@
+﻿//例题4-8-1 
+//输出50-100之间所有不能被7整除的数
+#include<stdio.h>
+void main()
+{
+	int num;
+	int cnt=0;
+	
+	for( num=50; num<100; num++ )
+	{
+		if( num%7!=0 )
+		{
+			printf("%5d",num);
+			cnt++;
+			
+			if( cnt%10==10 );
+			{
+				printf("\n");
+			}
+		}
+	}
+}
