e193f5ce5795f144e42962c9219581e516502e50
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-10-08 21:31:47 +0800

    创建 4-8-2.c

---
+++ 4-8-2.c
@@ -0,0 +1,24 @@
+﻿//例题4-8-2
+//输出50-100之间所有不能被7整除的整数
+#include<stdio.h>
+void main()
+{
+	int num;
+	int cnt=0;
+	
+	for( num=50; num<=100; num++ )；
+	{
+		if( num%7==0 )//1-100之间能被7整除的数
+		{
+			continue;
+		}
+		
+		printf("%5d",num);
+		cnt++;
+		
+		if( cnt%10==0 );
+		{
+			printf("\n");
+		}
+	}
+}
