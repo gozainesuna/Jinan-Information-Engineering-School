fe4fa7c0898e4c1fa5865f3ec77c5b33724bc7e6
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-10-11 22:07:08 +0800

    创建 4-10-1.c

---
+++ 4-10-1.c
@@ -0,0 +1,35 @@
+﻿//例题4-10
+//输出100-200之间的所有素数
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	int num;
+	int i;
+	int tag;
+	int m;
+	
+	for( num=100; num<=200; num++ )
+	{
+		tag = 1;
+		m = sqrt(num);
+		
+		for( i=2; i<=m; i++ )
+		{
+			if( num%i==0 )
+			{
+				tag = 0;
+				break;
+			}
+		}
+		
+		if( tag==1 )
+		{
+			printf("%d是素数\n",num);
+		}
+		else
+		{
+			printf("%d不是素数\n",num);
+		}
+	}
+}
