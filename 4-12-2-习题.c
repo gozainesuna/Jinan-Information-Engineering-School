e18b8760be2623cf42f9b1ca2a71cf8664d9e4d9
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-11-08 21:00:29 +0800

    更新 4-12-2-习题.c

--- 4-12-2-习题.c
+++ 4-12-2-习题.c
@@ -1,15 +1,22 @@
-﻿#include<stdio.h>
+﻿//习题 4-12-2
+/*
+	孙悟空大闹蟠桃园，每天吃掉前一天剩下的一半多一个
+  	到第N天的时候发现仅剩一个桃子
+  	请问第一天原本有多少桃子。N从键盘输入
+*/
+#include<stdio.h>
 void main()
 {
-	int=i;
-	int N;
-	int x=1;
+	int i,N,a,b=1;
 	
-	printf("请输入N的值：")
+	printf("请输入N的值：");
 	scanf("%d",&N);
-	for( i=	n-1; i>=1; i++ )
+	
+	for( i=N-1; i>=1; i-- )
 	{
-		x=(x+1)*2;
+		a=(b+1)*2;
+		b=a;
 	}
-	printf("第一天原来有%d个桃子。\n",x);
+	
+	printf("第一天原来有%d个桃子。\n");
 }
