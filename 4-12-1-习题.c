b093af87d8ca9c05789ae1cd1b296153fbff2ac6
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-11-08 20:54:53 +0800

    11/8

--- 4-12-1-习题.c
+++ 4-12-1-习题.c
@@ -8,14 +8,15 @@
 #include<stdio.h>
 void main()
 {
-	int a,N,x=1;
+	int i,N,x=1;
 	
 	printf("请输入N的值：");
 	scanf("%d",&N);
 	
 	for( i=N-1; i>=1; i-- )
 	{
-		x=(x=1)*2；
+		x=(x+1)*2;
 	}
-	printf("第一天原来有%d个桃子\n",x)
+	
+	printf("第一天原来有%d个桃子\n",x);
 }
