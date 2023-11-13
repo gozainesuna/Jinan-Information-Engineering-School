f6bc76b3115e171f7ed4e34761a382b8a7fefc62
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-11-13 20:46:01 +0800

    123

--- 4-14-2-习题.c
+++ 4-14-2-习题.c
@@ -1,4 +1,12 @@
-﻿#include<stdio.h>
+﻿//习题4-14-2
+
+/*
+	输入一个数字a和整数n,s=a+aa+aaa+a...a
+	最后一项为n个a。计算并输出s的值
+	eg:a=2,n=5时，s=2+22+222+2222+22222=24690
+*/
+
+#include<stdio.h>
 void main()
 {
 	int a,n,i,s=0,t;
@@ -15,5 +23,6 @@ void main()
 		s+=t;
 		t=t*10+a;
 	}
+	
 	printf("s=%d\n",s);
 }
