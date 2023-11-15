ec4011a7d56a13a530720cf6f4be30dc0bb162be
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-11-15 15:29:28 +0800

    创建 4-15-2 习题.c

---
+++ 4-15-2 习题.c
@@ -0,0 +1,20 @@
+﻿//习题4-15-2
+/*
+	用下面的公式求PI的近似值，直到最后一项小于0.0001为止
+*/
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	int b=1, sign=1;
+	float sum=0.0, t=1.0;
+	
+	while(fab(t)>=0.0001)
+	{
+		t=1.0/b;
+		sum+=t*sign;
+		sign=-sign;
+		b+=2;
+	}
+	printf("PI=%f\n",a*sum);
+}
