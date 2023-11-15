4a3f008c62d637fae1c05fcdcf347da04137f8d1
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-11-15 15:17:15 +0800

    更新 4-3 习题.c

--- 4-3 习题.c
+++ 4-3 习题.c
@@ -1,8 +1,12 @@
-﻿#include<stdio.h>
+﻿//习题4-3
+/*
+	一个数列是：3/5，5/7，7/9，9/11，求这个数列前20项的和
+*/
+#include<stdio.h>
 void main()
 {
 	int i;
-	float a=3.0,b=5.0,s=0.0;
+	float a=3.0, b=5.0, s=0.0;
 	
 	for( i=1; i<=20; i++ )
 	{
