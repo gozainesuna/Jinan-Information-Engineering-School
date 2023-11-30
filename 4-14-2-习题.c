147bea81d870eddc61ca35eb8a8b28a010665ce6
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-11-30 19:34:35 +0800

    更新 4-14-2-习题.c

--- 4-14-2-习题.c
+++ 4-14-2-习题.c
@@ -1,18 +1,31 @@
+<<<<<<< Updated upstream
 ﻿//习题4-14-2
 /*
 	输入一个数字a和整数n,s=a+aa+aaa+a...a
 	最后一项为n个a。计算并输出s的值
 	eg:a=2,n=5时，s=2+22+222+2222+22222=24690
+=======
+﻿//习题 4-14-2
+/*		 
+	输入一个数字a和一个整数n。s=a+aa+aaa+a...a
+	最后一项为n个a。计算并输出s的值
+	eg:当a=2,n=5时，s=2+22+222+2222+22222
+>>>>>>> Stashed changes
 */
 #include<stdio.h>
 void main()
 {
 	int a,n,i,s=0,t;
 	
+<<<<<<< Updated upstream
 	printf("a=");
 	scanf("%d",&a);
 	printf("n=");
 	scanf("%d",&n);
+=======
+	printf("a="); scanf("%d",&a);
+	printf("n="); scanf("%d",&n);
+>>>>>>> Stashed changes
 	
 	t=a;
 	
