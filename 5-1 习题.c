183e1f067ac18ccb70372c28610b1acc60e13739
Shepherd <62826887+gozainesuna@users.noreply.github.com>
2023-12-26 10:03:31 +0800

    创建 5-1 习题.c

---
+++ 5-1 习题.c
@@ -0,0 +1,19 @@
+﻿//习题5-1
+/*
+	从键盘上输入10个数，并将这10个数逆序输出
+*/
+#include<stdio.h>
+void main()
+{
+	int a[10],i;
+	
+	printf("“输入10个整数：\n");
+	for( i=0; i<=9; i++ )
+	{
+		scanf("%d",&a[i]);
+	}
+	for( i=9; i>=0; i-- )
+	{
+		printf("%d",a[i])
+	}
+}
